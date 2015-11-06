ad6bf1861c4363c832fa953a7f6fe71903d9b226
Neil.Tran <neil.tran@infonam.com>
2015-11-06 14:10:51 +0700

    6/11/2015 - 2:10

--- app/src/main/java/com/android/neiltran/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/android/neiltran/criminalintent/CrimeListFragment.java
@@ -1,9 +1,56 @@
 package com.android.neiltran.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by neil.tran on 11/5/2015.
  */
 public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+/*
+    private class CrimeAdapter extends RecyclerView.Adapter<Crime>{
+        private List<Crime> mCrime;
+
+        @Override
+        public Crime onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(Crime holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }*/
 }
